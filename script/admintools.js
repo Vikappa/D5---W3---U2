1f9cdf675e6850c0a8cd40bb8720897f0a4b9e1f
Vikappa <vincenzo.e.costantini@gmail.com>
2024-01-19 13:14:10 +0100

    Parte 1

--- script/admintools.js
+++ script/admintools.js
@@ -9,8 +9,33 @@ class newProdotto {
 
 }
 
+class prodottoRicevuto {
+    constructor(name, description, brand, imageUrl, price, _id = "not generated", userID = "not generated", createdAt = "not generated", updatedat = "not generated", __V = "not generated") {
+        this.name = name
+        this.description = description
+        this.brand = brand
+        this.imageUrl = imageUrl
+        this.price = price
+        this._id = _id
+        this.userID = userID
+        this.createdAt = createdAt
+        this.updatedat = updatedat,
+            this.__V = __V
+    }
 
-const sendProd = function () {
+    buildCard() {
+        return `<div class="card" style="width: 18rem;">
+<img src="${this.imageUrl}" class="card-img-top" alt="${this.description}">
+<div class="card-body">
+  <h5 class="card-title">${this.name}</h5>
+  <p class="card-text">${this.description} , ${this.brand}, €${this.price}</p>
+  <a href="#" class="btn btn-primary" onclick=deleteProd("${this._id}") >Elimina</a>
+</div>
+</div>`
+    }
+
+}
+const sendProd = function (prodottoDaInviare) { //Invia oggetto newProdotto
     fetch("https://striveschool-api.herokuapp.com/api/product/", {
         method: "POST",
         body: JSON.stringify(prodottoDaInviare),
@@ -22,9 +47,12 @@ const sendProd = function () {
         .then(response => response.json())
         .then(data => console.log(data))
         .catch(error => console.error('Errore:', error))
+
+    attualizzaProdottiScaricati()
+    riempiColonnaProdotti()
 }
 
-const deleteProd = function (idDaEliminare) {
+const deleteProd = function (idDaEliminare) { // deleta in base alla stringa id dell'oggetto ricevuto dal server
 
     fetch(`https://striveschool-api.herokuapp.com/api/product/${idDaEliminare}`, {
         method: "DELETE",
@@ -39,12 +67,77 @@ const deleteProd = function (idDaEliminare) {
                 throw new Error('Errore nella richiesta DELETE')
             }
         })
-        .then(data => console.log('Oggetto eliminato:', data))
         .catch(error => console.error('Errore:', error))
+    attualizzaProdottiScaricati()
+    riempiColonnaProdotti()
 }
 
-deleteProd("65aa54aa187e5c001814c6a8")
+const attualizzaProdottiScaricati = function () {
+    prodottiScaricati = []
+    fetch("https://striveschool-api.herokuapp.com/api/product/", {
+        headers: {
+            "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWFhMmY2MDE4N2U1YzAwMTgxNGM1ZjYiLCJpYXQiOjE3MDU2NTIwNjQsImV4cCI6MTcwNjg2MTY2NH0.xxf1hSNP01Dcmf12PU4LL1ffA0S89nYHE4SCxrhtkwY"
+        }
+    })
+        .then((response) => {
+            if (response.ok) {
+                return response.json()
+            } else {
+                throw new Error('errore nella chiamata')
+            }
+        })
+        .then((data) => {
+            for (let i = 0; i < data.length; i++) {
+                let newProdottoScaricato = new prodottoRicevuto(
+                    data[i].name,
+                    data[i].description,
+                    data[i].brand,
+                    data[i].imageUrl,
+                    data[i].price,
+                    data[i]._id,
+                    data[i].userID,
+                    data[i].createdAt,
+                    data[i].updatedat,
+                    data[i].__V,
+                )
+                prodottiScaricati.push(newProdottoScaricato)
+            }
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+}
 
+const sendProdotto = function (event) {
+    event.preventDefault()
+    let daInviare = new newProdotto(
+        document.getElementById('nomeProdotto').value,
+        document.getElementById('descrizioneProdotto').value,
+        document.getElementById('brandProdotto').value,
+        document.getElementById('urlImgProdotto').value,
+        document.getElementById('prezzoProdotto').value)
+    sendProd(daInviare)
+    riempiColonnaProdotti()
+}
+
+let prodottiScaricati = []
+
+
+const riempiColonnaProdotti = function () {
+    let stringa_prodotti_esistenti = ``
+    console.log(prodottiScaricati)
+    for (let e = 0; e < prodottiScaricati.length; e++) {
+        console.log("prova")
+        stringa_prodotti_esistenti = stringa_prodotti_esistenti + prodottiScaricati[e].buildCard()
+    }
+
+    document.getElementById('productsPanel').innerHTML = stringa_prodotti_esistenti
+}
+
+////////////////////////////////////////////////// COLLEGAMENTI DOM //////////////////////////////////////////////////////
+const colonnaProdotti = document.getElementById('productsPanel')
+const colonnaForm = document.getElementById('formPanel')
+////////////////////////////////////////////////// RIEMPIMENTO PRINCIPALE ///////////////////////////////////////////////
 fetch("https://striveschool-api.herokuapp.com/api/product/", {
     headers: {
         "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWFhMmY2MDE4N2U1YzAwMTgxNGM1ZjYiLCJpYXQiOjE3MDU2NTIwNjQsImV4cCI6MTcwNjg2MTY2NH0.xxf1hSNP01Dcmf12PU4LL1ffA0S89nYHE4SCxrhtkwY"
@@ -58,10 +151,24 @@ fetch("https://striveschool-api.herokuapp.com/api/product/", {
         }
     })
     .then((data) => {
-        console.log(data)
+        for (let i = 0; i < data.length; i++) {
+            let newProdottoScaricato = new prodottoRicevuto(
+                data[i].name,
+                data[i].description,
+                data[i].brand,
+                data[i].imageUrl,
+                data[i].price,
+                data[i]._id,
+                data[i].userID,
+                data[i].createdAt,
+                data[i].updatedat,
+                data[i].__V,
+            )
+            prodottiScaricati.push(newProdottoScaricato)
+        }
+        riempiColonnaProdotti()
     })
     .catch((err) => {
         console.log(err)
     })
-
 
--- script/script.js
+++ script/script.js
@@ -1,4 +1,4 @@
-class prodotto {
+class prodottoRicevuto {
     constructor(name, description, brand, imageUrl, price, _id = "not generated", userID = "not generated", createdAt = "not generated", updatedat = "not generated", __V = "not generated") {
         this.name = name
         this.description = description
@@ -11,6 +11,18 @@ class prodotto {
         this.updatedat = updatedat,
             this.__V = __V
     }
+
+    buildCard() {
+        return `<div class="card" style="width: 18rem;">
+        <img src="${this.imageUrl}" class="card-img-top" alt="${this.description}">
+        <div class="card-body">
+          <h5 class="card-title">${this.name}</h5>
+          <p class="card-text">${this.description} , ${this.brand}, ${this.price}</p>
+          <a href="#" class="btn btn-primary"><img src="../assets/media/addtocart.svg"></a>
+        </div>
+        </div>`
+    }
+
 }
 /////////////////////////////////////// VARIABILI COMUNI PAGINA ///////////////////////////////////////////////////////
 let adminPassword = "abc"
